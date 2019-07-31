354474a56cf5760fcd917ca2316ce376e2d42204
sebastianchristopher <christophersebastianjohnson@gmail.com>
2019-07-31 09:13:53 +0100

    first commit

--- ruby/makers-twitter.rb
+++ ruby/makers-twitter.rb
@@ -1,13 +1,95 @@
+# As a manager,
+#   so that I can keep up to date with what is being said about the
+#   company on social media
+#   I would like to receive a text message of tweets with #mackersacademy in the hashtag
+#   As a manager,
+#   so that I can see just relevant details
+#   I would only like to see original tweets with the text, user and creation time.
+# cj - I'm assuming that original tweets means excluding retweets
+
+# As a manager,
+# so that I never miss a tweet
+# I would like tweets to be texted on a regular schedule (every day or so)
+
+require 'pry'
 require 'twitter'
 require 'dotenv/load'
+require 'twilio-ruby'
+require 'yaml'
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+class TwitterBot
+  def initialize
+# "client" gives us access to the whole Twitter API
+    @twitter_client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+    end
+
+    @twilio_client = Twilio::REST::Client.new(
+        account_sid = ENV["TWILIO_ACCOUNT_SID"],
+        auth_token = ENV["TWILIO_AUTH_TOKEN"]
+      )
+  end
+
+  def send_text(to = '+447795537261‬',from = '+441827232034')
+    message = latest_tweet
+    @twilio_client.api.account.messages.create(
+      to: to,
+      from: from,
+      body: "#makersacademy was tagged in a tweet:\n\n#{message}"
+     )
+   end
+
+   def daily_update
+     write_file
+     tweets = format_tweets
+     tweets
+   end
+
+   private
+
+   def all_tweets_with_hashtag(hashtag)
+# tweets with #mackersacademy (-rt part of twitter's search to remove retweets)
+     @twitter_client.search("##{hashtag} -rt", count: 100, lang: 'en')
+   end
+# another way to remove retweets, using the Ruby twitter api
+# tweets = hashtag_makersacademy.select { |tweet| !tweet.retweeted_tweet? }
+
+  def tweets_organised
+    tweets = all_tweets_with_hashtag("makersacademy")
+    list_of_tweets = []
+    tweets.each{ |tweet|
+      list_of_tweets << {
+      tweet: tweet.full_text,
+      user: tweet.user.screen_name,
+      created: tweet.created_at
+      }
+    }
+    list_of_tweets
+  end
+
+  def latest_tweet
+    tweets = tweets_organised
+    "posted by #{tweets.first[:user]} on #{tweets.first[:created]}\n\n#{tweets.first[:tweet]}"
+  end
+
+  def write_file
+    File.write('tweets.yml', YAML.dump(tweets_organised))
+  end
+
+  def load_file
+    YAML.load_file('tweets.yml')
+  end
+
+  def format_tweets
+    tweets = load_file
+    formatted_tweets = []
+    tweets.each { |tweet|
+      formatted_tweets << "posted by #{tweets.first[:user]} on #{tweets.first[:created]}\n#{tweets.first[:tweet]}\n"
+    }
+  end
 end
-
-client.search("makersacademy").each { |r|
-  puts r.full_text
-}
+# in pry, ls Twitter::Tweets will list the methods for Tweet class
+binding.pry
